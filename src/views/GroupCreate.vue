ae85746dbcc18c73a45b06b80bd518faa8435193
Michelle <michelledewit1.mdw@gmail.com>
2020-04-20 12:01:28 +0200

    Added GroupCreate

---
+++ src/views/GroupCreate.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+<h1>Create group</h1>
+
+<input v-model="groepsnaam" placeholder="groepsnaam">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        data:{
+            groepsnaam: ""
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
