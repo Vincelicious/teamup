0fa448884b016e440c7e2eeeccf72c216fa096b3
Michelle <michelledewit1.mdw@gmail.com>
2020-04-20 14:09:59 +0200

    Added v-model (name, trainingsDays)

--- src/views/GroupCreate.vue
+++ src/views/GroupCreate.vue
@@ -1,18 +1,27 @@
 <template>
     <div>
-<h1>Create group</h1>
-
-<input v-model="groepsnaam" placeholder="groepsnaam">
-
-    </div>
+        <h1>Create group</h1>
+        <input type=text id="name" name="name" v-model="group.name" placeholder="groupname">
+        <div class="trainingsDays"> 
+            <input type="checkbox" id="maandag" value="maandag" v-model="checkedNames">
+                <input type="checkbox" id="dinsdag" value="dinsdag" v-model="checkedNames">
+                <input type="checkbox" id="woensdag" value="woensdag" v-model="checkedNames">
+        </div>
+            </div>
 </template>
 
 <script>
-    export default {
-        data:{
-            groepsnaam: ""
-        }
+export default {
+  data: () => {
+    return {
+      group: {
+        name: "",
+        trainingDays: []
+      }
     }
+  }
+}
+  
 </script>
 
 <style scoped>
