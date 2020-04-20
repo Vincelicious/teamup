d330552a32d0d972ba48a2f397225e1d9b25a33c
Toirkens,Dayo D <d.toirkens@student.fontys.nl>
2020-04-20 15:16:21 +0200

    Added a beginning for the grouplist

--- src/components/GroupList/GroupCard.vue
+++ src/components/GroupList/GroupCard.vue
@@ -1,7 +1,21 @@
 <template>
-  <div>
-    <h2>{{ group.name }}</h2>
-    <p>{{ group.km }}KM</p>
+  <!-- Grouplist -->
+  <div class="flex justify-center text-white">
+    <!-- Group Card -->
+    <div class="flex flex-col bg-gray-500 m-3 w-full h-48">
+      <div class="title flex flex-col bg-gray-700 ">
+        <h2>{{ group.name }}</h2>
+        <p>{{ group.level }}KM</p>
+      </div>
+      <!-- Group Info -->
+      <div class="flex bg-gray-600 ">
+        <div class="info">
+          <h2>{{ group.location }}</h2>
+          <p>{{ group.members }} / {{ group.groupmax }}KM</p>
+          <h2>{{ group.goal }}</h2>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
