f4d033d5fb013b4893fbd1b8598ccf81c1542307
Michelle <michelledewit1.mdw@gmail.com>
2020-04-21 11:07:31 +0200

    Added NavBar Groupcreate

--- src/views/GroupCreate.vue
+++ src/views/GroupCreate.vue
@@ -1,20 +1,55 @@
 <template>
-    <div>
-        <h1>Create group</h1>
-        <input type=text id="name" v-model="group.name" placeholder="groupname">
-        <p>Groupname: {{ group.name }}</p>
-        <div class="trainingsDays" > 
-            <input type="checkbox" id="maandag" value="Maandag" v-model="group.trainingDays">
-            <input type="checkbox" id="dinsdag" value="Dinsdag" v-model="group.trainingDays">
-            <input type="checkbox" id="woensdag" value="Woensdag" v-model="group.trainingDays">
-            <input type="checkbox" id="donderdag" value="Donderdag" v-model="group.trainingDays">
-            <input type="checkbox" id="vrijdag" value="Vrijdag" v-model="group.trainingDays">
-            <input type="checkbox" id="zaterdag" value="Zaterdag" v-model="group.trainingDays">
-            <input type="checkbox" id="zondag" value="Zondag" v-model="group.trainingDays">
-        </div>
+  <div>
+    <h1>Create group</h1>
+    <input type="text" id="name" v-model="group.name" placeholder="groupname" />
+    <p>Groupname: {{ group.name }}</p>
+    <div class="trainingsDays">
+      <input
+        type="checkbox"
+        id="maandag"
+        value="Maandag"
+        v-model="group.trainingDays"
+      />
+      <input
+        type="checkbox"
+        id="dinsdag"
+        value="Dinsdag"
+        v-model="group.trainingDays"
+      />
+      <input
+        type="checkbox"
+        id="woensdag"
+        value="Woensdag"
+        v-model="group.trainingDays"
+      />
+      <input
+        type="checkbox"
+        id="donderdag"
+        value="Donderdag"
+        v-model="group.trainingDays"
+      />
+      <input
+        type="checkbox"
+        id="vrijdag"
+        value="Vrijdag"
+        v-model="group.trainingDays"
+      />
+      <input
+        type="checkbox"
+        id="zaterdag"
+        value="Zaterdag"
+        v-model="group.trainingDays"
+      />
+      <input
+        type="checkbox"
+        id="zondag"
+        value="Zondag"
+        v-model="group.trainingDays"
+      />
+    </div>
 
-          <span>Trainingsdag {{ group.trainingDays }}</span>
-            </div>
+    <span>Trainingsdag {{ group.trainingDays }}</span>
+  </div>
 </template>
 
 <script>
@@ -25,12 +60,9 @@ export default {
         name: "",
         trainingDays: []
       }
-    }
+    };
   }
-}
-  
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
