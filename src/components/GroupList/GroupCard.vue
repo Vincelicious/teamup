754b9aaadaf3d6c486078c0bff8d54439f337794
Toirkens,Dayo D <d.toirkens@student.fontys.nl>
2020-04-21 11:47:24 +0200

    Further progress on groupcards

--- src/components/GroupList/GroupCard.vue
+++ src/components/GroupList/GroupCard.vue
@@ -3,16 +3,18 @@
   <div class="flex justify-center text-white">
     <!-- Group Card -->
     <div class="flex flex-col bg-gray-500 m-3 w-full h-48">
-      <div class="title flex flex-col bg-gray-700 ">
+      <div class="title flex flex-col items-start bg-gray-700  ">
         <h2>{{ group.name }}</h2>
-        <p>{{ group.level }}KM</p>
+        <p>{{ group.level }}</p>
       </div>
       <!-- Group Info -->
-      <div class="flex bg-gray-600 ">
-        <div class="info">
-          <h2>{{ group.location }}</h2>
-          <p>{{ group.members }} / {{ group.groupmax }}KM</p>
-          <h2>{{ group.goal }}</h2>
+      <div class="flex bg-gray-600 h-full items-end">
+        <div class="info flex">
+          <h2 class="m-2 bg-red-600">{{ group.location }} KM</h2>
+          <p class="m-2 bg-red-600">
+            {{ group.members }} / {{ group.groupmax }} members
+          </p>
+          <h2 class="m-2 bg-red-600">{{ group.goal }}</h2>
         </div>
       </div>
     </div>
