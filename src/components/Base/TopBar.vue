a63effdecdf50a0e81ff309ce5285abd34550620
Bram <bram_vos@hotmail.com>
2020-04-22 10:23:58 +0200

    Added TopBar component

---
+++ src/components/Base/TopBar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="flex justify-center items-center bg-gray-600 h-14 fixed w-screen">
+    <h1 class="text-white text-xl font-bold">{{ pageTitle }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    pageTitle: String
+  }
+};
+</script>
+
+<style>
+
+
+.h-14{
+    height: 56px;
+}
+
+.pt-14{
+    padding-top: 56px;
+}
+
+</style>
--- src/views/GroupList.vue
+++ src/views/GroupList.vue
@@ -1,20 +1,51 @@
 <template>
   <div>
-    <h1>Group List</h1>
-    <GroupCard v-for="group in groups" :key="group.id" :group="group" />
+    <TopBar pageTitle="Group List"></TopBar>
+    <div class="pt-14">
+      <GroupCard v-for="group in groups" :key="group.id" :group="group" />
+    </div>
   </div>
 </template>
 
 <script>
 import GroupCard from "@/components/GroupList/GroupCard.vue";
+import TopBar from "@/components/Base/TopBar.vue";
 
 export default {
   components: {
-    GroupCard
+    GroupCard,
+    TopBar
   },
   data() {
     return {
       groups: [
+        {
+          id: 1,
+          name: "Speedrunners",
+          level: "Advanced",
+          location: 3,
+          members: 3,
+          groupmax: 4,
+          goal: "Full Marathon"
+        },
+        {
+          id: 2,
+          name: "Slowrunners",
+          level: "Beginner",
+          location: 1,
+          members: 5,
+          groupmax: 6,
+          goal: "Quarter Marathon"
+        },
+        {
+          id: 3,
+          name: "Hardrunners",
+          level: "Intermediate",
+          location: 1,
+          members: 4,
+          groupmax: 5,
+          goal: "Half Marathon"
+        },
         {
           id: 1,
           name: "Speedrunners",
@@ -48,4 +79,6 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+
+</style>
