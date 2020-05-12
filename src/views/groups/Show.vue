f1f20e555543e170566671d045e963d99e7d061e
Vincelicious <hello@vincentvandeursen.com>
2020-05-12 11:37:08 +0200

    Added name and goal to group.show page

--- src/views/groups/Show.vue
+++ src/views/groups/Show.vue
@@ -1,17 +1,21 @@
 <template>
   <div>
-    <div>
-      <h2>Team Up Group</h2>
+    <div class="bg-primary-color text-center">
+      <h2 class="text-2xl text-white py-8">Team Up Group</h2>
     </div>
 
-    <div>
-      <div>Group</div>
-      <div>Scheme</div>
-      <div>Chat</div>
+    <div class="bg-darkened-color flex justify-evenly text-white">
+      <div class="py-4 flex-1 text-center">Group</div>
+      <div class="py-4 flex-1 text-center">Scheme</div>
+      <div class="py-4 flex-1 text-center">Chat</div>
     </div>
 
-    <div class="container mx-auto px-4">
-      <h3>{{ group }}</h3>
+    <div class="container mx-auto px-4 py-8">
+      <h3 class="text-white text-2xl mb-2">{{ group.name }}</h3>
+      <span class="text-white flex items-center">
+        <BaseIcon class="mr-2" name="target" width="16" height="16" />
+        {{ group.goal }}
+      </span>
     </div>
   </div>
 </template>
