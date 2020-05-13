65d35ecd32ad50031bd550a637e1f5b6e54609bf
Bram <bram_vos@hotmail.com>
2020-05-13 14:55:05 +0200

    Fixed db data, button at grouplist, topbar at group create

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -1,16 +1,16 @@
-import axios from 'axios'
-    
-const apiClient = axios.create({  
-  baseURL: `http://localhost:3000`,
+import axios from "axios";
+
+const apiClient = axios.create({
+  baseURL: `http://86.95.88.160:3000`,
   withCredentials: false, // This is the default
   headers: {
-    Accept: 'application/json',
-    'Content-Type': 'application/json'
+    Accept: "application/json",
+    "Content-Type": "application/json"
   }
-})
+});
 
 export default {
   getGroups() {
-    return apiClient.get('/groups')
+    return apiClient.get("/groups");
   }
-}
+};
