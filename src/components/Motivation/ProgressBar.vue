eb267ffac1d19752dbd0051a9822f2c86ee7ad0f
Michelle <michelledewit1.mdw@gmail.com>
2020-05-18 11:08:31 +0200

    Implemented progress bar component

---
+++ src/components/Motivation/ProgressBar.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="app"><h2>Jaap</h2></div>
+</template>
+<script></script>
+<style scoped></style>
---
+++ src/views/Progress.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="app">
+    <ProgressBar></ProgressBar>
+  </div>
+</template>
+<script>
+import ProgressBar from "@/components/Motivation/ProgressBar.vue";
+
+export default {  components: {
+    ProgressBar
+  }};
+</script>
+<style scoped></style>
