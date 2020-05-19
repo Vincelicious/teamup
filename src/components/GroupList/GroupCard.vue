dea7aef09f9dcd1d9e11395f840afef29a0f4099
Toirkens,Dayo D <d.toirkens@student.fontys.nl>
2020-05-19 13:54:52 +0200

    Displaying the data correctly for the new data formatting

--- src/components/GroupList/GroupCard.vue
+++ src/components/GroupList/GroupCard.vue
@@ -6,28 +6,27 @@
       <!-- Title -->
       <div class="flex justify-between">
         <h2 class="text-xl font-bold mb-2">{{ group.name }}</h2>
-        <ChevronRightIcon class="icon-color"/>
+        <ChevronRightIcon class="icon-color" />
       </div>
       <div class="flex">
-        <div class="card-training-days mr-2 " v-for="(available, day) in group.trainingdays" :key="day" :group="group" v-show="available">
+        <div class="card-training-days mr-2 " v-for="day in group.trainingDays" :key="day" :group="group">
           {{ day }}
         </div>
       </div>
- 
-
-
 
       <!-- Group Info -->
       <div class="group-info flex h-full items-end">
-       
         <div class="flex mr-5">
-          <UsersIcon class="card-icon icon-color"></UsersIcon><p class="min-w-8">{{ group.members }} / {{ group.groupmax }}</p>
+          <UsersIcon class="card-icon icon-color"></UsersIcon>
+          <p class="min-w-8">{{ group.members.length }} / {{ group.maxMembers }}</p>
         </div>
-         <div class="flex mr-5">
-          <MapPinIcon class="card-icon icon-color"></MapPinIcon><p>10 km</p>
+        <div class="flex mr-5">
+          <MapPinIcon class="card-icon icon-color"></MapPinIcon>
+          <p>10 km</p>
         </div>
         <div class="flex">
-          <TargetIcon class="card-icon icon-color"></TargetIcon><p>{{ group.goal }}</p>
+          <TargetIcon class="card-icon icon-color"></TargetIcon>
+          <p>{{ group.marathonId }}</p>
         </div>
       </div>
     </div>
@@ -35,21 +34,20 @@
 </template>
 
 <script>
-import { MapPinIcon, TargetIcon, UsersIcon, ChevronRightIcon  } from "vue-feather-icons";
+import { MapPinIcon, TargetIcon, UsersIcon, ChevronRightIcon } from "vue-feather-icons";
 
 export default {
   components: { MapPinIcon, TargetIcon, UsersIcon, ChevronRightIcon },
   props: {
-    group: Object
-  }
+    group: Object,
+  },
 };
 </script>
 
 <style scoped>
-
 /* Bottom Icons Styling */
 
-.card-icon{
+.card-icon {
   margin-right: 0.5em;
   height: 1.2em;
   width: 1.2em;
@@ -60,14 +58,14 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
+  text-transform: capitalize;
 
-  border: 1px solid theme('colors.highlighted-color');
+  border: 1px solid theme("colors.highlighted-color");
   border-radius: 6px;
   font-size: 10px;
 
   font-family: Lato;
   font-style: normal;
   font-weight: 500;
-  }
-
+}
 </style>
