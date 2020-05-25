6eaf167ecf0fc9ef41f04c10064233b6641bf12a
Michelle <michelledewit1.mdw@gmail.com>
2020-05-25 10:42:09 +0200

    Added Modal component

---
+++ src/components/Motivation/Modal.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="app"><h1>Test</h1></div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
--- src/views/Progress.vue
+++ src/views/Progress.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="app">
     <ProgressBar></ProgressBar>
+    <Modal></Modal>
   </div>
 </template>
 <script>
 import ProgressBar from "@/components/Motivation/ProgressBar.vue";
+import Modal from "@/components/Motivation/Modal.vue";
 
 export default {
   components: {
-    ProgressBar
+    ProgressBar,
+    Modal
   }
 };
 </script>
