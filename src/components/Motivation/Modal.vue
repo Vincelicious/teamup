71338bae2a0598d149b0d9cb93e24b3498e03888
Michelle <michelledewit1.mdw@gmail.com>
2020-05-26 13:33:15 +0200

    Added lottie

--- src/components/Motivation/Modal.vue
+++ src/components/Motivation/Modal.vue
@@ -6,6 +6,7 @@
           X
         </button>
         <p class="text-center text-2xl py-2">Total Kilometers</p>
+        <!-- <div id="total-kilometers-badge"></div> -->
         <p class="text-center text-lg py-2">3 Kilometers</p>
         <p class="text-center text-sm text-opacity-50 py-2">
           Your total numbers of kilometers ran
@@ -18,6 +19,7 @@
 
 <script>
 import ProgressBar from "@/components/Motivation/ProgressBar.vue";
+
 
 export default {
   components: {
@@ -32,14 +34,31 @@ export default {
   methods: {
     close() {
       this.$emit("input", !this.value);
-    }
+    },
+    animation() {}
   }
+  // mounted() {
+  //   let externalScript = document.createElement("script");
+  //   externalScript.setAttribute(
+  //     "src",
+  //     "C:/xampp/htdocs/ict/semester4/teamup/src/assets/badges/lottie.js"
+  //   );
+  //   document.head.appendChild(externalScript);
+  // }
 };
+
+// var totalKilometersBadge = bodymovin.loadAnimation({
+//   container: document.getElementById("total-kilometers-badge"),
+//   renderer: "svg",
+//   loop: true,
+//   autoplay: true,
+//   path: "../../assets/badges/total-kilometers-badge.json"
+// });
 </script>
 
 <style lang="css">
 .modal {
-  background-color: rgba(0, 0, 0, 0.7);
+  background-color: theme("colors.darkened-color");
   position: absolute;
 }
 </style>
--- src/views/Progress.vue
+++ src/views/Progress.vue
@@ -2,15 +2,18 @@
   <div class="app">
     <button class="btn text-white" @click="openModal">Open modal</button>
     <Modal v-model="modalOpen"></Modal>
+    <LottiePoc></LottiePoc>
     <h1>Test</h1>
   </div>
 </template>
 <script>
 import Modal from "@/components/Motivation/Modal.vue";
+import LottiePoc from "@/components/Motivation/LottiePoc.vue";
 
 export default {
   components: {
-    Modal
+    Modal,
+    LottiePoc
   },
   data() {
     return {
