7eb652fe4501ac19a2076a91cedaf1fef6615b9d
Bram <bram_vos@hotmail.com>
2020-06-08 14:40:57 +0200

    Fixed styling

--- src/components/Motivation/Modal.vue
+++ src/components/Motivation/Modal.vue
@@ -1,35 +1,36 @@
 <template>
-  <div class="app flex justify-center">
-    <div class="modal w-11/12" v-show="value">
-      <div class="container flex flex-col ">
-        <button @click.prevent="close" class="text-white ">
-          X
-        </button>
-        <p class="text-center text-2xl py-2">Total Kilometers</p>
+  <div class="app flex justify-center content-center bg" v-show="value">
+    <div class="modal w-11/12" >
+      <div class="container flex flex-col">
+       
+        <p class="text-center text-2xl pt-2 pb-6 font-bold ">Fastest Kilometer</p>
         <lottie
           :options="defaultOptions"
           :height="200"
           :width="200"
           v-on:animCreated="handleAnimation"
         />
-        <p class="text-center text-lg py-2">3/5 Kilometers</p>
-        <p class="text-center text-sm text-opacity-50 py-2">
-          Your total numbers of kilometers ran
+        <p class="text-center text-2xl py-2 font-bold">00 : 05 : 43</p>
+        <p class="text-center text-lg text-white opacity-50 py-2">
+          Your fastest kilometer.
         </p>
-        <ProgressBar></ProgressBar>
+        <!-- <ProgressBar></ProgressBar> -->
+         <button @click.prevent="close" class="text-white button">
+          <p class="font-bold text-lg">Close</p>
+        </button>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import ProgressBar from "@/components/Motivation/ProgressBar.vue";
+// import ProgressBar from "@/components/Motivation/ProgressBar.vue";
 import Lottie from "vue-lottie";
 import animationData from "@/assets/badges/total-kilometers-badge.json";
 
 export default {
   components: {
-    ProgressBar,
+    // ProgressBar,
     lottie: Lottie
   },
   name: "app",
@@ -66,5 +67,25 @@ export default {
 .modal {
   background-color: theme("colors.primary-color");
   position: absolute;
+  top: 80px;
+  border-radius: 10px;
+  padding: 30px;
+
+}
+
+.button{
+  width: 200px;
+  margin: 35px auto 0px auto;
+  background:theme("colors.highlighted-color");
+  border-radius: 10px;
+  height: 50px;
+}
+
+.bg{
+  position: absolute;
+  top: 56px;
+  background: theme("colors.darkened-color");
+  width: 100vw;
+  height: calc(100vh - 56px);
 }
 </style>
