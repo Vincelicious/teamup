3d9d72f3bd8dd0bd0a8310a769cb09e58e158dd5
JordyBushoff <Jordy_bushoff@Hotmail.com>
2020-06-10 15:06:20 +0200

    Feature/filter Gemaakt

---
+++ src/views/MyGroup.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <GroupFilter></GroupFilter>
+    </div>
+</template>
+
+<script>
+
+import GroupFilter from "@/components/GroupList/GroupFilter.vue";
+
+export default {
+  components: {GroupFilter}
+
+};
+</script>
+
+<style>
+
+</style>
