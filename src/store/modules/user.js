af2418a64470a943fab1d8b44831d0bc381a09e1
Vincelicious <hello@vincentvandeursen.com>
2020-06-10 14:46:50 +0200

    A random user is now set on load

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,18 +1,25 @@
 import UserService from "@/services/UserService";
 
 const state = {
+  user: {},
   users: []
 };
 
 const getters = {
   getUserById: state => id => {
     return state.users.find(user => user.id == id);
+  },
+  getRandomUser: state => {
+    return state.users[Math.floor(Math.random() * state.users.length)];
   }
 };
 
 const mutations = {
   SET_USERS(state, users) {
     state.users = users;
+  },
+  SET_USER(state, user) {
+    state.user = user;
   }
 };
 
@@ -25,6 +32,9 @@ const actions = {
       .catch(error => {
         console.error(error);
       });
+  },
+  setRandomUser({ commit, getters }) {
+    commit("SET_USER", getters.getRandomUser);
   }
 };
 
