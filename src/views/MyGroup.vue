a5af9e0af2c74e01742c1e37eae51dc57d65258f
Vincelicious <hello@vincentvandeursen.com>
2020-06-10 14:28:47 +0200

    Added top bar to my group view

---
+++ src/views/MyGroup.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div class="bg-primary-color text-center">
+      <h2 class="top-bar text-xl text-white flex justify-center items-center">
+        Team Up Group
+      </h2>
+    </div>
+
+    <div class="bg-darkened-color flex justify-evenly text-white">
+      <div class="py-3 flex-1 text-center border-b-4 border-highlighted-color">
+        Group
+      </div>
+      <div class="py-3 flex-1 text-center">Scheme</div>
+      <div class="py-3 flex-1 text-center">Chat</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.top-bar {
+  height: 56px;
+}
+</style>
