643780bcba346f99c06932249202010f5cf40b9f
Toirkens,Dayo D <d.toirkens@student.fontys.nl>
2020-06-10 10:23:21 +0200

    Converted the recordbadge component to single divs and made them clickable

--- src/views/Progress.vue
+++ src/views/Progress.vue
@@ -1,47 +1,77 @@
 <template>
   <div class="app">
-
     <TopBar pageTitle="Progress"></TopBar>
     <!-- Wat is de beste manier om de content naar beneden te pushen? Dit hieronder kan veranderd worden -->
     <div class="spacing py-8"></div>
-
-    <div class="recordbadges flex justify-center mb-3">
-      <RecordBadge 
-        v-for="badge in recordBadges"
-        :key="badge.id"
-        :recordBadges="badge"
-      ></RecordBadge>
+    <div class="container clickable">
+      <div class="recordbadges flex flex-start mb-3">
+        <div class="badge1" @click="openLongestRun">
+          <div class="container  px-5">
+            <div class="text-white flex flex-col items-center">
+              <div class="badge-icon flex justify-center items-center">
+                <img src="@/assets/badges/Longest-Run-Badge.svg" />
+              </div>
+              <div class="badge-info flex flex-col justify-center mb-3 ml-3">
+                <div>
+                  <h1 class="font-bold mt-3">Longest Run</h1>
+                  <p class="underline-text text-center">
+                    12.54 km
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="badge2" @click="openFastestKm">
+          <div class="container clickable px-5">
+            <div class="text-white flex flex-col items-center">
+              <div class="badge-icon flex justify-center items-center">
+                <img src="@/assets/badges/Fastest-Km-Badge.svg" />
+              </div>
+              <div class="badge-info flex flex-col justify-center mb-3 ml-3">
+                <div>
+                  <h1 class="font-bold mt-3">Fastest Km</h1>
+                  <p class="underline-text text-center">
+                    04:54
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="badge3" @click="openTarget">
+          <div class="container clickable px-5">
+            <div class="text-white flex flex-col items-center">
+              <div class="badge-icon flex justify-center items-center">
+                <img src="@/assets/badges/Target-Badge.svg" />
+              </div>
+              <div class="badge-info flex flex-col justify-center mb-3 ml-3">
+                <div>
+                  <h1 class="font-bold mt-3">Best Finish</h1>
+                  <p class="underline-text text-center">
+                    04:32:54
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
 
     <Badge></Badge>
     <Badge></Badge>
     <Badge></Badge>
 
-
-
-    <button class="btn text-white" @click="openFastestKm">
-      Open ModalFastestKm
-    </button>
-    
     <ModalFastestKm
       v-model="ModalFastestKm"
       ref="loadFastestKm"
     ></ModalFastestKm>
-
-    <button class="btn text-white" @click="openTarget">
-      Open ModalTarget
-    </button>
     <ModalTarget v-model="ModalTarget" ref="loadTarget"></ModalTarget>
-
-    <button class="btn text-white" @click="openLongestRun">
-      Open LongestRun
-    </button>
-
     <ModalLongestRun
       v-model="ModalLongestRun"
       ref="loadLongestRun"
     ></ModalLongestRun>
-    <h1>Test</h1>
   </div>
 </template>
 <script>
@@ -50,15 +80,14 @@ import ModalFastestKm from "@/components/Motivation/ModalFastestKm.vue";
 import ModalLongestRun from "@/components/Motivation/ModalLongestRun.vue";
 import TopBar from "@/components/Base/TopBar.vue";
 import Badge from "@/components/Base/Progress/Badge.vue";
-import RecordBadge from "@/components/Base/Progress/RecordBadge.vue";
+// import RecordBadge from "@/components/Base/Progress/RecordBadge.vue";
 import { mapState } from "vuex";
-
 
 export default {
   components: {
     TopBar,
     Badge,
-    RecordBadge,
+    // RecordBadge,
     ModalTarget,
     ModalFastestKm,
     ModalLongestRun
@@ -97,4 +126,14 @@ export default {
   }
 };
 </script>
-<style scoped></style>
+<style scoped>
+.underline-text {
+  color: #9d9d9d;
+}
+.badge-icon {
+  width: 20vw;
+}
+.clickable {
+  cursor: pointer;
+}
+</style>
