28db12d000281e86908373acd0bb4305d0af32eb
Toirkens,Dayo D <d.toirkens@student.fontys.nl>
2020-06-16 13:09:07 +0200

    Readded navbar

---
+++ src/components/Base/ProgressNavBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div class="bg-darkened-color flex justify-evenly text-white mb-10">
+      <div class="py-4 flex-1 text-center border-b-4 border-highlighted-color">
+        Badge
+      </div>
+      <!-- All the information about the runs -->
+      <div class="py-4 flex-1 text-center">Statistics</div>
+      <!-- Previous trainings -->
+      <div class="py-4 flex-1 text-center">History</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
